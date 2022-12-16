26091667e88418de6f312bf93e93d93dea5b9e29
VirginiaMaria <virginiamaria54321@gmail.com>
2022-12-16 11:05:45 -0300

    up

---
+++ src/components/formulario.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <View>
+      <Text>Altura (m)</Text>
+      <TextInput
+      placeholder="Ex: 1.72"
+      keyboardType="numeric"
+      />
+      <Text>Peso (kg)</Text>
+      <TextInput
+      placeholder="Ex: 80.5"
+      keyboardType="numeric"
+      />
+    </View>
+  )
+}
+
+export default Formulario
+
+const styles = StyleSheet.create({})
